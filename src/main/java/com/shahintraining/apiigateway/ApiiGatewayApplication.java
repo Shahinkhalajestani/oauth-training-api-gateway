ade7d51b8b178dd20166cc89317fd0170d9bbd00
sh.khalajestanii <NiH@sh123>
2021-12-25 11:03:46 +0330

    First Commit (two routes for services)

---
+++ src/main/java/com/shahintraining/apiigateway/ApiiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shahintraining.apiigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahintraining/apiigateway/ApiiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahintraining.apiigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
